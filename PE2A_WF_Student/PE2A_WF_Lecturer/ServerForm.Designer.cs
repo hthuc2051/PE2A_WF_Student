42c1a8384982129bd220118acf7c62b50793abf2
Lam Thanh Phat <lamthanhphat98@gmail.com>
2020-01-21 19:25:08 +0700

    server sends file to clients through socket ( TCP / IP )

---
+++ PE2A_WF_Student/PE2A_WF_Lecturer/ServerForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PE2A_WF_Lecturer
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClient = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClient
+            // 
+            this.btnClient.Location = new System.Drawing.Point(228, 142);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(321, 45);
+            this.btnClient.TabIndex = 0;
+            this.btnClient.Text = "Open client form";
+            this.btnClient.UseVisualStyleBackColor = true;
+            this.btnClient.Click += new System.EventHandler(this.btnClient_Click);
+            // 
+            // ServerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClient);
+            this.Name = "ServerForm";
+            this.Text = "ServerForm";
+            this.Load += new System.EventHandler(this.ServerForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClient;
+    }
+}
