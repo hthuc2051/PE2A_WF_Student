5442a39fca6040ff9e72f59c2776c704470adc28
nhuythuc <nhthuc1502@gmail.com>
2020-02-07 10:14:15 +0700

    Change port

--- PE2A_WF_Student/PE2A_WF_Student/Constant.cs
+++ PE2A_WF_Student/PE2A_WF_Student/Constant.cs
@@ -8,8 +8,8 @@ namespace PE2A_WF_Student
 {
     public class Constant
     {
-        public static int LECTURER_LISTENING_PORT = 5656;
-        public static int STUDENT_LISTENING_PORT = 4000;
+        public static int LECTURER_LISTENING_PORT = 9999;
+        public static int STUDENT_LISTENING_PORT = 9998;
         public static int MAXIMUM_REQUEST = 100;
         public static string EXISTED_IP_MESSAGE = "You Have Connected To Server";
         public static string[] STATUSLIST = { "connected","submitted","checked","returned"};
