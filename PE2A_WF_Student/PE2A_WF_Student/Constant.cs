fa9b8b46c19b6cad576e65ce765d02b1df517e26
Nhat Thien <thienvo0211@gmail.com>
2020-02-20 17:22:44 +0700

    [SUBMISSION] Fix update submission time

--- PE2A_WF_Student/PE2A_WF_Student/Constant.cs
+++ PE2A_WF_Student/PE2A_WF_Student/Constant.cs
@@ -14,6 +14,6 @@ namespace PE2A_WF_Student
         public static string EXISTED_IP_MESSAGE = "You Have Connected To Server";
         public static string[] STATUSLIST = { "connected","submitted","checked","returned"};
         public static string CANNOT_LOGIN_ADMIN_MESSAGE = "You are not permitted to log in with this role";
-        public static string SUBMMIT_SUCCESS_MESSAGE = "success";
+        public static string SUBMMIT_SUCCESS_MESSAGE = "Submit successfully";
     }
 }
